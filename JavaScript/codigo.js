42b9dc79a1d8288954eace63fd6a01f8505baa81
BelucaNM <beluca.navarrina@gmail.com>
2024-04-03 00:53:44 +0200

    fin noche

--- JavaScript/codigo.js
+++ JavaScript/codigo.js
@@ -18,25 +18,38 @@ else {alert ("adios es mayor ")}
 
 var valores = [true, 5, false, "hola", "adios", 2];
 
-var elMayor = "";
-var elNumero1 =0;
-var elNumero2 =0;
+var elMayor = ""
+var elNumero1 =0
+var elNumero2 =0
+var elBoolean1 = false
+var elBoolean2 = false
 var encontradoPrimero = false
+var encontradoBoolean = false
 for (let $i=0; $i< valores.length ;$i++){ 
      if ((typeof valores[$i] == 'string') && (valores[$i]> elMayor))
      {elMayor = valores[$i]}
      if  ( typeof valores[$i] == 'number') 
         if ( !encontradoPrimero )   { elNumero1 = valores[$i]; encontradoPrimero = true; }
         else { elNumero2 = valores[$i];}
+    if  ( typeof valores[$i] == 'boolean') 
+        if ( !encontradoBoolean )   { elBoolean1 = valores[$i]; encontradoBoolean = true; }
+        else { elBoolean2 = valores[$i];}
     }
+
+
 console.log ( "el String mayor es "+ elMayor)
 console.log ( "la Suma de los numeros es =" + (elNumero1 + elNumero2))
 console.log ( "la resta de los numeros es =" + (elNumero1 - elNumero2))
 console.log ( "El producto de los numeros es =" + (elNumero1 * elNumero2))
+console.log ( "La division de los numeros es =" + (elNumero1 / elNumero2))
+console.log ( "El " + elNumero1 + "% de " + elNumero2 +" es =" + (elNumero1 % elNumero2))
 
-
-
-
+console.log ("El Bolean1 es " + elBoolean1)
+console.log ("El Bolean2 es " + elBoolean2)
+var elResultado = elBoolean1 || elBoolean2
+if (elResultado) {console.log ("Con OR el resultado es " + elResultado)}
+elResultado = elBoolean1 && elBoolean2
+if (!elResultado) {console.log ("Con AND el resultado es " + elResultado)}
 
 
 // Ejercicio 5
