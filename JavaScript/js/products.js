363b59f743684c2b4609fec149642f8d9e5033d2
BelucaNM <beluca.navarrina@gmail.com>
2024-04-05 14:04:02 +0200

    fin mañana

---
+++ JavaScript/js/products.js
@@ -0,0 +1,51 @@
+function getProducts() {
+    let products = [
+        {
+            name: "peres", 
+            category: "fruites",
+            price: 1.5
+        },
+        {
+            name: "Xai", 
+            category: "carns",
+            price: 1.56
+        },
+        {
+            name: "pomes", 
+            category: "fruites",
+            price: 1.5
+        },
+        {
+            name: "Pollastre", 
+            category: "carns",
+            price: 1.68
+        },
+        {
+            name: "Tonyina", 
+            category: "peixos",
+            price: 5.68
+        },
+
+        {
+            name: "Conill", 
+            category: "carns",
+            price: 1.68
+        },
+        {
+            name: "Llobarro", 
+            category: "peixos",
+            price: 5.68
+        },
+        {
+            name: "kiwi", 
+            category: "fruites",
+            price: 1.25
+        },
+        {
+            name: "Porc", 
+            category: "carns",
+            price: 3.68
+        }
+    ];
+    return products;
+}
